fef52310b2ad4c73b3128b8c1c9c5cd49d746c18
Yuri Cruz <yuricruzf@gmail.com>
2024-02-11 23:16:34 -0300

    Getting errors from subscribe

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -14,9 +14,15 @@ export class ListaLivrosComponent {
   constructor(private service: BookService) { }
 
   getTheBooks() {
-    this.service.getBooks(this.searchField).subscribe((returnFromAPI) => {
-      console.log(returnFromAPI);
-    })
+    this.service.getBooks(this.searchField).subscribe(
+      // (returnFromAPI) => console.log(returnFromAPI), (error) => { console.log(error); } //DESATUALIZADA
+      //Novo método para passar parâmetros para o subscribe
+      {
+        next: returnFromAPI => console.log(returnFromAPI), //Poder ser emitido vários valores durante sua existência
+        error: erro => console.error(erro), //Opcional - Encerra o ciclo de vida
+        complete: () => console.log('Observable completado') //Opcional - Encerra o ciclo de vida
+      }
+    )
   }
 }
 
