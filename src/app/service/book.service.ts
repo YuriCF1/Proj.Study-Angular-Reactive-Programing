6f4068f39f80e26a16f988a7f670c400b6d94a69
Yuri Cruz <yuricruzf@gmail.com>
2024-02-14 16:29:09 -0300

    Using map and tap methods

--- src/app/service/book.service.ts
+++ src/app/service/book.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
-import { Livro, LivrosResultados } from '../models/interfaces';
+import { Observable, map, tap } from 'rxjs';
+import { Item, Livro, LivrosResultados } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +13,12 @@ export class BookService {
 
   constructor(private http: HttpClient) { }
 
-  getBooks(valueTyped: string): Observable<LivrosResultados> {
+  getBooks(valueTyped: string): Observable<Item[]> {
     const params = new HttpParams().append('q', valueTyped)
     return this.http.get<LivrosResultados>(this.API, { params }).pipe(//Deve também ser tipada no get, não apenas na função da requisição
-      tap<LivrosResultados>(retornoAPI => console.log(retornoAPI))
+      tap<LivrosResultados>(retornoAPI => console.log('Antes do map', retornoAPI)),
+      map(resultado => resultado.items),
+      tap(resultado => console.log('Fluxo após o map:', resultado))
     )
   }
 }
@@ -38,4 +40,15 @@ observable.pipe(
   filter(value => value > 1),
   tap(value => console.log('Depois do filtro:', value))
 ).subscribe();
+*/
+
+
+/*
+Um Operador é uma função que cria um novo Observable com base no Observable atual.
+Esta é uma operação pura: o Observable anterior permanece inalterado.
+O pipe agrupa operadores, não modifica o fluxo inicial de dados retornados.
+
+Operadores são funções variadas que permitem combinar rapidamente diferentes observables
+para obter diferentes tipos de resultados. O operador tap não vai modificar o observable anterior, por isso,
+diferentemente dos outros operadores, não precisamos nos preocupar com a ordem dele.
 */
