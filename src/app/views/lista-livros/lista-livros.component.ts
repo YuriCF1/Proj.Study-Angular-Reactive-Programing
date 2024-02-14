b2edc12957b6a2b4f0a67966622e7c7ceb02a7b0
Yuri Cruz <yuricruzf@gmail.com>
2024-02-14 17:49:45 -0300

    Isolating each item from api

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/models/interfaces';
 import { BookService } from 'src/app/service/book.service';
 
 @Component({
@@ -9,10 +10,10 @@ import { BookService } from 'src/app/service/book.service';
 })
 export class ListaLivrosComponent implements OnDestroy {
 
-  listaLivros: [];
+  listaLivros: Livro[];
   searchField: string = '';
-
   subscription: Subscription
+  livro: Livro
 
   constructor(private service: BookService) { }
 
@@ -21,17 +22,35 @@ export class ListaLivrosComponent implements OnDestroy {
       // (returnFromAPI) => console.log(returnFromAPI), (error) => { console.log(error); } //DESATUALIZADA
       //Novo método para passar parâmetros para o subscribe
       {
-        next: returnFromAPI => console.log(returnFromAPI), //Poder ser emitido vários valores durante sua existência
+        // next: returnFromAPI => console.log(returnFromAPI), //Poder ser emitido vários valores durante sua existência
+        next: itens => {
+          this.listaLivros = this.livrosResultadoParaLivros(itens)
+        },
         error: erro => console.error(erro), //Opcional - Encerra o ciclo de vida
         complete: () => console.log('Observable completado') //Opcional - Encerra o ciclo de vida
       }
     )
   }
 
+  livrosResultadoParaLivros(items): Livro[] {
+    const livros: Livro[] = []
+    items.forEach(item => {
+      livros.push(this.livro = {
+        title: item.volumeInfo?.title,
+        authors: item.volumeInfo?.authors,
+        publisher: item.volumeInfo?.publisher,
+        publishedDate: item.volumeInfo?.publishedDate,
+        description: item.volumeInfo?.description,
+        previewLink: item.volumeInfo?.previewLink,
+        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
+      })
+    });
+    return livros
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe()
   }
-
 }
 
 
