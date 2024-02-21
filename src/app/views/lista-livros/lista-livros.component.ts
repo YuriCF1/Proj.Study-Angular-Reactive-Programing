dd85f20e53b499aca020006c3a31d1c94b347070
Yuri Cruz <yuricruzf@gmail.com>
2024-02-21 14:08:21 -0300

    Taking notes

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -125,4 +125,12 @@ que atrasa as emissões do Observable de origem pelo tempo especificado.
 distinctUntilChanged - Operador de filtragem. Retorna um Observable que emite todos os valores enviados
 pelo observable de origem se forem distintos em comparação com o último valor emitido pelo observable de resultado.
 
+catchError - Operador de Tratamento de Erros. Captura erros no observable manipulado retornando um novo observable ou lançando um erro.
+
+throwError - Operador de Criação. Cria um observable que criará uma instância de erro e a enviará ao consumidor como um erro imediatamente após a assinatura.
+
+EMPTY - Operador de Criação. Cria um Observable simples que não emite itens para o Observer e imediatamente emite uma notificação de complete.
+
+of - Operador de Criação. Converte os argumentos em observable. Um Observable que emite os argumentos descritos e depois conclui.
+
 */
